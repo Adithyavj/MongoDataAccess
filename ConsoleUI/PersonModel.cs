11446ee398f51fa2e34ce0b81bbc75531cae7ba4
Adithya Vijay K <adithyavj@gmail.com>
2022-02-02 19:53:17 +0530

    Add new ConsoleUI with connection to mongodb database

---
+++ ConsoleUI/PersonModel.cs
@@ -0,0 +1,14 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace ConsoleUI;
+
+public class PersonModel
+{
+    // Data is stored in MongoDB as Bson (Binary Javascript Object Notation)
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+}
---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,22 @@
+﻿using MongoDB.Driver;
+using ConsoleUI;
+
+// Adding DB connection..
+string connectionString = "mongodb://localhost:27017";
+string databaseName = "simple_db";
+string collectionName = "people";
+
+var client = new MongoClient(connectionString); // connection to MongoDB
+var db = client.GetDatabase(databaseName); // connection to our Database in MongoDB
+var collection = db.GetCollection<PersonModel>(collectionName); // Getting value from collection in the Database
+
+var person = new PersonModel { FirstName = "Tim", LastName = "Corey" };
+
+await collection.InsertOneAsync(person); // insert values to db
+
+var results = await collection.FindAsync(_ => true);
+
+foreach (var result in results.ToList())
+{
+    Console.WriteLine($"{ result.Id}: {result.FirstName} {result.LastName}");
+}
