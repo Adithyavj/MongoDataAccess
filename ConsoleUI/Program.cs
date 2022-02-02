95dfa18dadd6b0dfcf4eec23dfeb942c3ba747b2
Adithya Vijay K <adithyavj@gmail.com>
2022-02-02 22:41:53 +0530

    Adding calls to insert and read values

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,22 +1,40 @@
-﻿using MongoDB.Driver;
-using ConsoleUI;
+﻿//using MongoDB.Driver;
+//using ConsoleUI;
 
-// Adding DB connection..
-string connectionString = "mongodb://localhost:27017";
-string databaseName = "simple_db";
-string collectionName = "people";
+//// Adding DB connection..
+//string connectionString = "mongodb://localhost:27017";
+//string databaseName = "simple_db";
+//string collectionName = "people";
 
-var client = new MongoClient(connectionString); // connection to MongoDB
-var db = client.GetDatabase(databaseName); // connection to our Database in MongoDB
-var collection = db.GetCollection<PersonModel>(collectionName); // Getting value from collection in the Database
+//var client = new MongoClient(connectionString); // connection to MongoDB
+//var db = client.GetDatabase(databaseName); // connection to our Database in MongoDB
+//var collection = db.GetCollection<PersonModel>(collectionName); // Getting value from collection in the Database
 
-var person = new PersonModel { FirstName = "Tim", LastName = "Corey" };
+//var person = new PersonModel { FirstName = "Tim", LastName = "Corey" };
 
-await collection.InsertOneAsync(person); // insert values to db
+//await collection.InsertOneAsync(person); // insert values to db
 
-var results = await collection.FindAsync(_ => true);
+//var results = await collection.FindAsync(_ => true);
 
-foreach (var result in results.ToList())
-{
-    Console.WriteLine($"{ result.Id}: {result.FirstName} {result.LastName}");
-}
+//foreach (var result in results.ToList())
+//{
+//    Console.WriteLine($"{ result.Id}: {result.FirstName} {result.LastName}");
+//}
+
+using MongoDataAccess.DataAccess;
+using MongoDataAccess.Models;
+
+ChoreDataAccess db = new ChoreDataAccess();
+
+await db.CreateUser(new UserModel { FirstName = "Tim", LastName = "Corey" });
+
+var users = await db.GetAllUsers();
+
+var chore = new ChoreModel 
+{ 
+    AssignedTo = users.First(), 
+    ChoreText = "Mow the lawn", 
+    FrequencyInDays = 7 
+};
+
+await db.CreateChore(chore);
