ec878d1770d8edd38679d6d899280a5875f3024f
Adithya Vijay K <adithyavj@gmail.com>
2022-02-02 14:39:24 +0530

    intial commit

---
+++ MongoDBDemo/Program.cs
@@ -0,0 +1,27 @@
+﻿using MongoDB.Driver;
+using System;
+
+namespace MongoDBDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MongoCRUD db = new MongoCRUD("AddressBook");
+            Console.ReadLine();
+        }
+    }
+
+    public class MongoCRUD
+    {
+        // creating a connection to db
+        private IMongoDatabase db;
+
+        public MongoCRUD(string database)
+        {
+            // initialise and connect to the database
+            var client = new MongoClient();
+            db = client.GetDatabase(database);
+        }
+    }
+}
